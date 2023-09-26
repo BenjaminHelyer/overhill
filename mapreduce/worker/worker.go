c1a0029e8b1ec2cc0d94512115968289ecba3f23
BenjaminHelyer <91896754+BenjaminHelyer@users.noreply.github.com>
2023-09-26 16:04:41 -0500

    Filled out all initial tests for the worker process.

--- mapreduce/worker/worker.go
+++ mapreduce/worker/worker.go
@@ -7,3 +7,16 @@ func readFromFile(filepath string) string {
 func writeToFile(filepath string) {
 	return
 }
+
+type MapFunc func(string, string) (string, string)
+
+func runMapFunc(userFunc MapFunc, input_key string, input_val string) (string, string) {
+	return "", ""
+}
+
+// TODO: make the second argument an iterator rather than an array
+type ReduceFunc func(string, [10]string) (string, [2]string)
+
+func runReduceFunc(userFunc ReduceFunc, input_key string, input_vals [10]string) (string, [2]string) {
+	return "", [2]string{"", ""}
+}
--- mapreduce/worker/worker_test.go
+++ mapreduce/worker/worker_test.go
@@ -7,7 +7,7 @@ import (
 
 // Worker should read from file
 func TestWorkerReadFile(t *testing.T) {
-	filepath := "test_resources\\test_worker.json"
+	filepath := "test_resources\\map_input.json"
 	output := readFromFile(filepath)
 	if output != "this is a test" {
 		t.Errorf("File output = %v; want 'this is a test'", output)
@@ -15,9 +15,19 @@ func TestWorkerReadFile(t *testing.T) {
 	}
 }
 
+func MockMapFunc(string, string) (string, string) {
+	return "", ""
+}
+
 // Worker should run Map functions
 func TestWorkerRunMapFunc(t *testing.T) {
-
+	input_key := ""
+	input_val := ""
+	output_key, output_val := runMapFunc(MockMapFunc, input_key, input_val)
+	if output_key != "test" || output_val != "test" {
+		t.Errorf("Did not receive expected output from running the provided map function.")
+		t.Fail()
+	}
 }
 
 // Worker should write to local disk
@@ -33,15 +43,36 @@ func TestWorkerWriteDisk(t *testing.T) {
 
 // Worker should read from (possibly remote) disk
 func TestWorkerReadRemoteDisk(t *testing.T) {
+	filepath := "test_resources\\reduce_input.json"
+	output := readFromFile(filepath)
+	if output != "this is a test" {
+		t.Errorf("File output = %v; want 'this is a test'", output)
+		t.Fail()
+	}
+}
 
+func MockReduceFunc(string, [10]string) (string, [2]string) {
+	return "", [2]string{"", ""}
 }
 
 // Worker should run Reduce functions
 func TestWorkerRunReduceFunc(t *testing.T) {
-
+	input_key := ""
+	input_vals := [10]string{"", "", "", "", "", "", "", "", "", ""}
+	output_key, output_vals := runReduceFunc(MockReduceFunc, input_key, input_vals)
+	if output_key != "test" || output_vals != [2]string{"test", "test"} {
+		t.Errorf("Did not receive expected output from running the provided reduce function.")
+		t.Fail()
+	}
 }
 
 // Worker should write to output filesystem
 func TestWorkerWriteOutputFilesystem(t *testing.T) {
-
+	filepath := "test_resources\\worker_output.json"
+	writeToFile(filepath)
+	_, err := os.Stat(filepath)
+	if err != nil {
+		t.Errorf("Error upon attempting to read output file: %v", err)
+		t.Fail()
+	}
 }
