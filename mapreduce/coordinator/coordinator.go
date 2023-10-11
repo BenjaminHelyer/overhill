f6225a47df67603a038ce53f2d5aba66ec21356c
BenjaminHelyer <91896754+BenjaminHelyer@users.noreply.github.com>
2023-10-10 22:00:50 -0500

    Began filling out RunMapWorkers for multi-threaded worker behavior. Also added in one more text to test storage.

--- mapreduce/coordinator/coordinator.go
+++ mapreduce/coordinator/coordinator.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"sync"
+	"time"
 )
 
 type Coordinator struct {
@@ -100,14 +102,66 @@ func (c *Coordinator) PartitionFolder(folderPath string, taskType string) error 
 }
 
 func (c *Coordinator) RunMapWorkers(mapFunc string, inputFolder string, intermediateFolder string) error {
+	// desired behavior:
+	// assign as many partition tasks as we can at the beginning
+	// check on all assigned workers every X seconds (where X is a design parameter)
+	// if assigned worker has completed, mark task as done
+	// hand it another task if there are remaining tasks
+	// if assigned worker hasn't completed, wait another Y iterations (where Y is a design parameter),
+	// then if it still hasn't completed, mark the worker as failed and assign the task to another worker
+
+	// error variable to return
+	var mapError error
+	mapError = nil
+
+	// initialize stack of partitions needing a worker with all partitions
+	var partitionsNeedWorker []string
+	for partition := range c.mapPartitionStatus {
+		partitionsNeedWorker = append(partitionsNeedWorker, partition)
+	}
+
+	// initialize stack of idle workers needing a task
+	var idleWorkers []string
+	for worker := range c.workerStatus {
+		idleWorkers = append(idleWorkers, worker)
+	}
+
+	// TODO: implement a thread to assign incomplete tasks to idle workers
+	// there should also be one thread for each assigned task
+
+	// thread to check on all partitions
+	// the process will wait on this thread to finish before
+	// either exiting or moving onto the reduce step
+	var mapWait sync.WaitGroup
+	allDone := false
+	iteration := 0
+	go func(partitionsNeedWorker []string) {
+		for allDone != true || iteration < 10 {
+			time.Sleep(1) // TODO: change this into a design parameter somewhere
+			allDone = true
+			for _, status := range c.mapPartitionStatus {
+				// in the go task for each worker request, we'll mark the task as complete
+				if status != "complete" {
+					allDone = false
+					// TODO: determine if this worker if failed.
+					// if it has, mark the partition status as incomplete and put it back on the stack
+				}
+			}
+			iteration++
+		}
+		// TODO: update mapError on non-recoverable failures
+		// only move onto Reduce when the above loop terminates
+		defer mapWait.Done()
+	}(partitionsNeedWorker)
+
 	var firstPartition string
 	for partition := range c.mapPartitionStatus {
 		firstPartition = partition
 		break
 	}
 
+	// TODO: handle faults if a particular Map task doesn't complete
 	for workerUrl := range c.workerStatus {
-		// TODO: rather than combining here, let's store the path to the partition in the partition map
 		response, workerErr := SendMapRequest(workerUrl, mapFunc, inputFolder+firstPartition, intermediateFolder+"test"+"_intermediate.json")
 
 		// TODO: rather than raising here, mark the worker as bad and assign the map task to another worker
@@ -118,7 +172,9 @@ func (c *Coordinator) RunMapWorkers(mapFunc string, inputFolder string, intermed
 			return workerErr
 		}
 	}
-	return nil
+
+	mapWait.Wait()
+	return mapError
 }
 
 func (c *Coordinator) RunReduceWorkers(reduceFunc string, intermediateFolder string) error {
