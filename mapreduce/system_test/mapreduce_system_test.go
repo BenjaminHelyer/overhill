349ad09007149777b22a9a2e9abc3c024b871843
BenjaminHelyer <91896754+BenjaminHelyer@users.noreply.github.com>
2023-10-10 21:15:52 -0500

    Added multi worker test which runs correctly and initially fails as expected.

--- mapreduce/system_test/mapreduce_system_test.go
+++ mapreduce/system_test/mapreduce_system_test.go
@@ -139,6 +139,136 @@ func TestEmersonWordCount_SingleWorker(t *testing.T) {
 	}
 }
 
+func TestEmersonWordCount_MultiWorker(t *testing.T) {
+	coordConfigFile := "../coordinator/test_resources/hardcoded_workers.json"
+	emersonFolder := "test_storage/emerson/"
+
+	expectedFinalOutput := []worker.KeyValue{
+		{"word", "9985"},
+	}
+
+	// timeout to ensure the test doesn't run forever
+	timeout := 2 * time.Second
+	t.Parallel() // allow this to run in parallel with other tests
+
+	timeoutCh := make(chan struct{})
+	go func() {
+		time.Sleep(timeout + 1)
+		timeoutCh <- struct{}{}
+	}()
+
+	var wg sync.WaitGroup
+	var workerProcesses []*exec.Cmd
+	var coordProcess *exec.Cmd
+	var coordError error
+
+	workerArgs := [][]string{{"--port=5060"}, {"--port=5070"}, {"--port=5080"}}
+	// need to ensure the worker process starts before the coordinator
+	workerStarted := make([]chan struct{}, len(workerArgs))
+	for i := range workerStarted {
+		workerStarted[i] = make(chan struct{})
+	}
+
+	for i, arg := range workerArgs {
+		go func(i int, arg []string) {
+			workerProcess, workerError := runSubprocess("../main.exe", arg)
+			if workerError != nil {
+				t.Errorf("Error running worker subprocess: %v", workerError)
+				t.Fail()
+			}
+			workerProcesses = append(workerProcesses, workerProcess)
+			close(workerStarted[i])
+		}(i, arg)
+	}
+
+	// wait for all worker processes to start
+	for i := range workerStarted {
+		<-workerStarted[i]
+	}
+
+	coordinatorArgs := []string{"--coord", "--config=" + coordConfigFile, "--mapFunc=wc_total", "--reduceFunc=wc_total", "--input=" + emersonFolder}
+	wg.Add(1)
+	go func() {
+		coordProcess, coordError = runSubprocess("../main.exe", coordinatorArgs)
+		if coordError != nil {
+			t.Errorf("Error running coordinator subprocess: %v", coordError)
+			t.Fail()
+		}
+
+		coordError := coordProcess.Wait()
+		if coordError != nil {
+			t.Errorf("Error running coordinator subprocess: %v", coordError)
+			t.Fail()
+		}
+
+		defer wg.Done()
+	}()
+
+	wg.Wait()
+
+	expectedFinalResultPath := "test_final.json"
+	expectedIntermediateResultsPath := "intermediate/test_intermediate.json"
+
+	_, fileExistsError := os.Stat(expectedFinalResultPath)
+	if fileExistsError != nil {
+		t.Errorf("Final output file does not exist: %v", fileExistsError)
+		t.Fail()
+	}
+
+	finalFile, fileOpenError := os.Open(expectedFinalResultPath)
+	if fileOpenError != nil {
+		t.Errorf("Error upon opening final output file: %v", fileOpenError)
+		t.Fail()
+	}
+
+	var finalOutput []worker.KeyValue
+	decoder := json.NewDecoder(finalFile)
+	if decodeErr := decoder.Decode(&finalOutput); decodeErr != nil {
+		t.Errorf("Error upon decoding output file: %v", decodeErr)
+		t.Fail()
+	}
+
+	for index, val := range expectedFinalOutput {
+		if finalOutput[index] != val {
+			t.Errorf("Final output does not match expected on a given key: %v, found values %v != %v", index, val, finalOutput[index])
+			t.Fail()
+		}
+	}
+
+	finalFile.Close()
+	removeErr := os.Remove(expectedFinalResultPath)
+	if removeErr != nil {
+		t.Errorf("Error deleting final output")
+	}
+
+	os.Remove(expectedIntermediateResultsPath)
+	if removeErr != nil {
+		t.Errorf("Error deleting intermediate output")
+	}
+
+	defer func() {
+		for _, workerProcess := range workerProcesses {
+			if workerProcess != nil && workerProcess.Process != nil {
+				print("Killing worker processes...\n")
+				killErr := workerProcess.Process.Kill()
+				if killErr != nil {
+					t.Errorf("Error upon trying to kill worker process: %v", killErr)
+					t.Fail()
+				}
+			}
+		}
+	}()
+
+	select {
+	case <-timeoutCh:
+		// test completed within timeout bounds
+		print("Test completed within timeout bounds.\n")
+	case <-time.After(timeout):
+		t.Errorf("Test timed out after %v [s]", timeout)
+		t.Fail()
+	}
+}
+
 func runSubprocess(name string, args []string) (*exec.Cmd, error) {
 	cmd := exec.Command(name, args...)
 	cmd.Stdout = os.Stdout
